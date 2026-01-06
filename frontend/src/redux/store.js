5044543434402b3588397c933965950950d27c24
Kleriz171 <bibakleris@gmail.com>
2026-01-06 16:34:21 +0100

    Add redux

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit"
+import  customerSlice  from "./slices/customerSlice"
+
+const store = configureStore({
+    reducer: {
+        customer: customerSlice
+    }
+});
+
+export default store;
