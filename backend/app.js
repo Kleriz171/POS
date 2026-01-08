2e14637c3a7a4d06b3bd8f92a700f91db4d4d73a
Kleriz171 <bibakleris@gmail.com>
2026-01-08 18:49:11 +0100

    Add global error handler

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,22 @@
 const config = require("./config/config")
 const express = require('express');
-const connectDB = require("./config/database")
+const connectDB = require("./config/database");
+const globalErrorHandler = require("./middleware/globalErrorHandler");
+const createHttpError = require("http-errors")
 const app = express();
 
 const PORT = config.PORT;
 connectDB()
 
 app.get("/", (req, res)=>{
+
+    const err = createHttpError(404, "smth went wrong")
+    throw err
     res.json({message: "Hello from POS server"})
 })
+
+
+app.use(globalErrorHandler)
 
 app.listen(PORT, ()=>{
     console.log("Listening on PORT:" + PORT)
--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 
 const config = Object.freeze({
-    port:process.env.PORT || 3000,
+    PORT: process.env.PORT || 3000,
     MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://kleri:kleri@pos-cluster.9ridmvu.mongodb.net/?appName=pos-cluster",
     nodeEnv : process.env.NODE_ENV || "development"
 })
