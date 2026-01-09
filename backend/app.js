7193d7cb4b91dd03dcff5b557a38d58c3ad89279
Kleriz171 <bibakleris@gmail.com>
2026-01-09 15:34:08 +0100

    Add routes for user management

--- backend/app.js
+++ backend/app.js
@@ -5,19 +5,28 @@ const globalErrorHandler = require("./middleware/globalErrorHandler");
 const createHttpError = require("http-errors")
 const app = express();
 
+//routes
+const userRoutes = require("./routes/userRoute")
+
 const PORT = config.PORT;
+//db
 connectDB()
 
+//routes
 app.get("/", (req, res)=>{
 
     const err = createHttpError(404, "smth went wrong")
     throw err
     res.json({message: "Hello from POS server"})
 })
+app.use("/api/user", userRoutes)
 
 
+//error handler
 app.use(globalErrorHandler)
 
+
+//server start
 app.listen(PORT, ()=>{
     console.log("Listening on PORT:" + PORT)
 })
---
+++ backend/routes/userRoute.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const router = express.router()
+
+
+router.route("/register").post();
+router.route("login").post()
+
+
+module.exports = router
