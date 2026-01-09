72aec1841a3f227cac708eee288039d34080a4f8
Kleriz171 <bibakleris@gmail.com>
2026-01-09 16:37:49 +0100

    Add user login functionality

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,8 +2,9 @@ require("dotenv").config()
 
 const config = Object.freeze({
     PORT: process.env.PORT || 3000,
-    MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://kleri:kleri@pos-cluster.9ridmvu.mongodb.net/?appName=pos-cluster",
-    nodeEnv : process.env.NODE_ENV || "development"
+    MONGODB_URI: process.env.MONGODB_URI,
+    nodeEnv : process.env.NODE_ENV || "development",
+    accesTokenSecret: process.env.accesTokenSecret
 })
 
 module.exports = config
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,35 +1,75 @@
 const createHttpError = require("http-errors")
 const User = require("../models/userModel")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const config = require("../config/config")
 
-const register = async (req,res,next) =>{
-    try{
-        const {name, phone, email, password, role} = req.body
-        if(!name|| !phone|| !email|| !password|| !role){
+const register = async (req, res, next) => {
+    try {
+        const { name, phone, email, password, role } = req.body
+        if (!name || !phone || !email || !password || !role) {
             const error = createHttpError(400, "All fields must be filled")
-            next(error )
+            next(error)
         }
-        const isUserPresent = await User.findOne({email})
-        if(isUserPresent){
+        const isUserPresent = await User.findOne({ email })
+        if (isUserPresent) {
             const error = createHttpError(400, "User already exists")
             next(error)
         }
 
-        const user = {name, phone, email, password, role};
+        const user = { name, phone, email, password, role };
         const newUser = User(user)
         await newUser.save()
 
-        res.status(201).json({success: true, message: "New user created", data: newUser})
+        res.status(201).json({ success: true, message: "New user created", data: newUser })
 
 
-    }catch(error){
+    } catch (error) {
         next(error)
     }
 
 
 }
 
-const login = async (req,res,next) =>{
+const login = async (req, res, next) => {
+    try {
 
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            const error = createHttpError(400, "all fields are required!")
+            next(error)
+        }
+        const isUser = await User.findOne({ email })
+        if (!isUser) {
+            const error = createHttpError(401, "Invalid Credentials")
+            next(error)
+        }
+
+        const isMatch = await bcrypt.compare(password, isUser.password)
+        if (!isMatch) {
+            const error = createHttpError(401, "Invalid Credentials")
+            next(error)
+        }
+
+        const accesToken = jwt.sign({_id: isUser._id}, config.accesTokenSecret, {
+            expiresIn: "20d"
+        })
+
+        res.cookie('accesToken', accesToken, {
+            maxAge: 1000 * 60 * 60 * 24 * 30,
+            httpOnly: true,
+            sameSite: 'none',
+            secure: true
+        })
+        res.status(200).json({succes:true, message: "User login successfully!",
+            data: isUser
+        })
+
+    } catch (error) {
+        next(error)
+    }
 }
 
-module.exports = { register, login}
+
+module.exports = { register, login }
