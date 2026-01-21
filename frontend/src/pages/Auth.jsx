24f29108635fd6f3ca83e5ba65a28d3c9dc336e9
Kleriz171 <bibakleris@gmail.com>
2026-01-21 16:23:34 +0100

    Did a lot prolly

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,23 +1,48 @@
 import restaurant from "../assets/images/restaurant.jpg"
+import logo from "../assets/images/logo.png"
 
 
 function Auth() {
     return (
         <div className="flex min-h-screen w-full">
             {/* Left section */}
-            <div className="w-1/2 relative flex items-center justify-center bg-cover">
-                <img src={restaurant} alt="Restaurant image" />
+            <div
+                className="hidden md:flex w-3/4 relative items-center justify-center bg-cover bg-center"
+                style={{ backgroundImage: `url(${restaurant})` }}
+            >
+                <div className="absolute inset-0 bg-black/80 z-10"></div>
 
-                <div className="absolute inset-0 bg-black bg-opacity-80"></div>
-
-                <blockquote className="absolute bottom-10 px-8 mb-10 text-2xl italic text-white">
+                <blockquote className="absolute bottom-10 px-8 mb-10 text-2xl italic text-white z-20">
                     "There is no love sincerer than the love of food"
-                    <br />
                     <span className="block mt-4 text-yellow-400">
-                        -Founder of Restaurant
+                        - Founder of Restaurant
                     </span>
                 </blockquote>
+            </div>
+
+            {/* Right section */}
+            <div className="w-1/4 min-h-screen bg-[#1a1a1a] p-10">
+                <div className="flex flex-col items-center gap-2">
+                    <img src={logo} alt="Resto Logo" className="h-14 w-14 border-2 rounded-full p-1" />
+                    <h1 className="text-lg font-semibold text-[#f5f5f5]">Restro</h1>
+                </div>
+                <h2 className="text-4xl text-center mt-10 font-semibold text-yellow-400 mb-10">
+                    Employee Registration
+                </h2>
+
+                {/* Components */}
+                {/* Register */}
+
+                <div className="flex justify-center mt-6">
+                    <p className="text-sm text-[#ababab]">
+                        Already have an account 
+                        <a className="pl-1 text-yellow-400 font-semibold hover:underline" href="#">
+                            Sign in
+                        </a>
+                    </p>
+                </div>
             </div>
         </div>)
 }
 export default Auth;
+
