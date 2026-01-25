9e1d5b6e90b1820cb6978411b81521d2a8659acb
Kleriz171 <bibakleris@gmail.com>
2026-01-25 14:05:46 +0100

    Add loading screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,31 @@
-import { BrowserRouter as Router, Routes, Route, Form, useLocation } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from "react-router-dom"
 import { Home, Orders, Menu, Auth } from "./pages"
 import Tables from "./pages/tables"
 import Header from "./components/shared/Header"
 import BottomNav from "./components/shared/BottomNav"
+import { useSelector } from "react-redux"
+import useLoadData from "./hooks/useLoadData"
+import FullScreenLoader from "./components/shared/FullScreenLoader"
 
 function Layout() {
 
   const location = useLocation();
+  const isLoading = useLoadData()
   const hideHeaderRoutes = ["/auth"]
+  const { isAuth } = useSelector(state => state.user)
+
+  if(isLoading) return<FullScreenLoader/>
 
 
   return (
     <>
         {!hideHeaderRoutes.includes(location.pathname) && <Header />}
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/tables" element={<Tables />} />
-          <Route path="/menu" element={<Menu />} />
+          <Route path="/" element={<ProtectedRoutes><Home /></ProtectedRoutes>} />
+          <Route path="/auth" element={isAuth ? <Navigate to="/"/> : <Auth/>} />
+          <Route path="/orders" element={<ProtectedRoutes><Orders /></ProtectedRoutes>} />
+          <Route path="/tables" element={<ProtectedRoutes><Tables /></ProtectedRoutes>} />
+          <Route path="/menu" element={<ProtectedRoutes><Menu /></ProtectedRoutes>} />
           <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
         {!hideHeaderRoutes.includes(location.pathname) && <BottomNav />}
@@ -27,7 +34,14 @@ function Layout() {
 
 }
 
+function ProtectedRoutes({children}) {
+  const {isAuth} = useSelector(state => state.user)
+  if(!isAuth){
+    return <Navigate to="/auth" />
+  }
 
+  return children
+}
 
 function App() { 
   return(
